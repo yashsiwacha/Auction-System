aabb3944223ba693173062a24bff26a7bd4b3f04
Yash Siwach <yashsiwach07@gmail.com>
2025-07-07 13:37:05 +0530

    Initial commit

---
+++ src/test/java/com/lpu/AuctionSystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpu.AuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
